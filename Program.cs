cdbf021fa8dcdb4070831392f58d4b8bc276cd21
Sławomir Majchrzak <52676366+smmaya@users.noreply.github.com>
2021-06-29 14:18:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pętle_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("podaj liczbę wierszy: ");
+            int wiersze = Convert.ToInt32(Console.ReadLine());
+            Console.Write("podaj liczbę gwiazdek: ");
+            int gwiazdki = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= wiersze; i++)
+            {
+                for (int j = 1; j <= gwiazdki; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
